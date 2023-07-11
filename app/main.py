81413e6069ed37f3e69e98ea747b39f18888038d
yurii <benisones@gmail.com>
2023-07-11 12:09:21 +0300

    work

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import os
+from faker import Faker
+
+
+def show_greeting() -> [str]:
+    fake = Faker()
+    name = fake.name()
+    address = fake.address()
+
+    print(f"Hello, my name is {name}!\nI live in {address}\n")
+
+
+def show_current_directory() -> [str]:
+    cur_directory = os.getcwd()
+    print(f"We work in {cur_directory}")
+
+
+def main():
+    show_greeting()
+    show_current_directory()
